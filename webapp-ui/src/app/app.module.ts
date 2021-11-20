80f7f80e5b1e8d88ef75466109ce32437a05bb87
huuthien0512 <huuthien0512@gmail.com>
2021-11-20 21:24:49 +0700

    Init table

--- webapp-ui/src/app/app.module.ts
+++ webapp-ui/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManagementComponent } from './management/management.component';
+import { TableComponent } from './management/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagementComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
